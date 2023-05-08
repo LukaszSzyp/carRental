a7310919239011ed1d0874015f28adad5e0f6201
Lukasz Szypulewski <szyp86@gmail.com>
2023-05-08 11:37:41 +0200

    install and config translation

--- carRental-web/src/App.tsx
+++ carRental-web/src/App.tsx
@@ -1,5 +1,21 @@
+import { useTranslation } from "react-i18next";
+import { useEffect } from "react";
+
 function App() {
-  return <></>;
+  const { t, i18n } = useTranslation();
+
+  useEffect(() => {
+    const lng = navigator.language;
+    i18n.changeLanguage(lng);
+  }, []);
+
+  const lng = navigator.language;
+  return (
+    <>
+      <h1>{t("cars")}</h1>
+      <p>jezyk {lng}</p>
+    </>
+  );
 }
 
 export default App;
